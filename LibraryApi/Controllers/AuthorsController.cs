58130bd30869960cbb031c9954e9f8cf91ede6e6
Natalia <nkuderewicz@gmail.com>
2026-06-10 14:43:13 +0200

    authors controller

--- LibraryApi/Controllers/AuthorsController.cs
+++ LibraryApi/Controllers/AuthorsController.cs
@@ -27,6 +27,51 @@ public class AuthorsController : ControllerBase
     {
         _db.Authors.Add(author);
         _db.SaveChanges();
+        return CreatedAtAction(nameof(GetAll), new { id = author.Id }, author);
+        //return StatusCode(201, author);
+        //return Ok(author); //200 - Ok
+    }
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Author author)
+    {
+        var existingAuthor = _db.Authors.Find(id);
+
+        if (existingAuthor == null)
+        {
+            return NotFound();
+        }
+
+        existingAuthor.FirstName = author.FirstName;
+        existingAuthor.LastName = author.LastName;
+
+        _db.SaveChanges();
+
+        return NoContent();
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var author = _db.Authors.Find(id);
+
+        if (author == null)
+            return NotFound();
+
         return Ok(author);
     }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var author = _db.Authors.Find(id);
+
+        if (author == null)
+            return NotFound();
+
+        _db.Authors.Remove(author);
+        _db.SaveChanges();
+
+        return NoContent(); // 204
+    }
+
+
 }
--- LibraryApi/Models/Author.cs
+++ LibraryApi/Models/Author.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace LibraryApi.Models;
 
 public class Author
 {
+
+
+    [JsonPropertyName("id")]
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+
+    [Required]
+    [JsonPropertyName("first_name")]
+    public string? FirstName { get; set; }
+
+    [Required]
+    [JsonPropertyName("last_name")]
+    public string? LastName { get; set; }
+
     public ICollection<Book> Books { get; set; } = new List<Book>();
 }
+
+
